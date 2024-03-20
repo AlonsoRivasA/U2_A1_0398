0f4a9a21792569ec67a667567d024ed1719483d0
LUIS_ALONSO_RIVAS_ANDRADE <a.21308051280398@cbtis128.edu.mx>
2024-03-20 15:45:18 +0000

    ac

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:rivas0398/pantallas0398/panel0398/panel_pantalla0398.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiAppXbox0398());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiAppXbox0398 extends StatelessWidget {
+  const MiAppXbox0398({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: "Rivas web",
       theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      home: const PanelPantalla0398(),
     );
   }
 }
